4dc98f791a4451b6ac80983b545077f641da2521
rafaelabimanyu <rafaelabimanyu333@gmail.com>
2026-05-09 16:53:46 +0700

    git commit -m "fix(build): repair package dependencies tailwind config and sidebar errors"

---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const forms = require('@tailwindcss/forms')
+
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './storage/framework/views/*.php',
+    ],
+
+    darkMode: 'class',
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+                jakarta: ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
+            },
+
+            colors: {
+                premium: {
+                    950: '#0a0d14',
+                    900: '#0f131d',
+                    800: '#1a202c',
+                    700: '#2d3748',
+                }
+            },
+
+            boxShadow: {
+                premium:
+                    '0 10px 15px -3px rgba(0,0,0,0.05), 0 4px 6px -2px rgba(0,0,0,0.025)',
+            },
+        },
+    },
+
+    plugins: [forms],
+}

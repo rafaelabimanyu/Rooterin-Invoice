366f2aefe53f6302a2960277f49eb7915d327416
rafaelabimanyu <rafaelabimanyu333@gmail.com>
2026-06-02 10:44:41 +0700

    chore(git): untrack framework storage compiled views and update .gitignore
    - Remove compiled Blade views and internal framework sessions from Git index
    - Update root .gitignore to permanently exclude temporary files under storage/framework/
    - Clear current view cache state using native artisan commands
    - Clean up Source Control changes layout to focus purely on core source code

--- storage/framework/views/02b0a1468e0e4624dda48c1628c1bc66.php
+++
@@ -1,5 +0,0 @@
-<thead class="sticky z-10 top-0 bg-white dark:bg-gray-900">
-    <?php echo e($slot); ?>
-
-</thead>
-<?php /**PATH C:\laragon\www\Rooterin-Invoice\vendor\laravel\pulse\src/../resources/views/components/thead.blade.php ENDPATH**/ ?>
--- storage/framework/views/df9ab7aeb716d0b6a641625a376b0852.php
+++
@@ -1,5 +0,0 @@
-<table <?php echo e($attributes->merge(['class' => 'w-full'])); ?>>
-    <?php echo e($slot); ?>
-
-</table>
-<?php /**PATH C:\laragon\www\Rooterin-Invoice\vendor\laravel\pulse\src/../resources/views/components/table.blade.php ENDPATH**/ ?>
